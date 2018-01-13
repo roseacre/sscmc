181e0204655946640880a46550ffb0d82d7f9f24
Fiona Evans <f.e.evans@btinternet.com>
2018-01-13 15:16:35 +0000

    Refactor diary page to have reusable components

--- public/pages/ssmcc-diary.php
+++ public/pages/ssmcc-diary.php
@@ -18,62 +18,19 @@ include '../templates/head-subpage.php';
         </div>
         <div class="col-md-10">
 
-            <div class="table-responsive">
-                <table class="table table-bordered table-striped dataTable" id="eventsDiary"
-                       role="grid"
-                       summary="This table provides details of services and events that may be of interest to members and visitors of
-                            South Shore Methodist Community Church.">
-                    <thead>
-                    <th id="header1" class="table-date-column sorting_asc" tabindex="0"
-                        aria-controls="eventsDiary" aria-sort="ascending"
-                        aria-label="Date: activate to sort column descending">Date
-                    </th>
-                    <th id="header2" class="table-time-column sorting" tabindex="0"
-                        aria-controls="eventsDiary" aria-sort="ascending"
-                        aria-label="Time: activate to sort column descending">Time
-                    </th>
-                    <th id="header3" class="table-location-column sorting" tabindex="0"
-                        aria-controls="eventsDiary" aria-sort="ascending"
-                        aria-label="Location: activate to sort column descending">Location
-                    </th>
-                    <th id="header4" class="table-notes-column2 sorting" tabindex="0"
-                        aria-controls="eventsDiary" aria-sort="ascending"
-                        aria-label="Event details: activate to sort column descending">Event Details
-                    </th>
-                    </thead>
-                    <tbody>
-                    <?php include '../snippets/lindale-diary-snippet.php'; ?>
-                    <?php include '../snippets/highfield-diary-snippet.php'; ?>
-                    </tbody>
-                </table>
-
+            <?php include '../snippets/diary-table-header.php'; ?>
+            <?php include '../snippets/lindale-diary-snippet.php'; ?>
+            <?php include '../snippets/highfield-diary-snippet.php'; ?>
+            <?php include '../snippets/diary-table-footer.php'; ?>
 
             </div>
         </div>
     </div>
+
     <?php include '../templates/footer.php'; ?>
 
-
     <?php include '../templates/scripts-subpage.php'; ?>
-
-    <script>
-        $(document).ready(function () {
-            $('#eventsDiary').dataTable({
-                "lengthChange": false,
-                "searching": false,
-                "columnDefs": [ {
-                    "targets": [ 0 ],
-                    "orderData": [ 0, 1 ]
-                }, {
-                    "targets": [ 1 ],
-                    "orderData": [ 1, 0 ]
-                }, {
-                    "targets": [ 2 ],
-                    "orderData": [ 2, 0, 1 ]
-                } ]
-            });
-        });
-    </script>
+    <?php include '../templates/tables-script-subpage.php'; ?>
 
 </body>
 </html>
