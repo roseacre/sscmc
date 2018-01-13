2c8f538a6d98cb708dab2e6b4231a7a2f6b55fb4
Fiona Evans <f.e.evans@btinternet.com>
2018-01-13 14:58:31 +0000

    Add diary of events for Lindale

---
+++ public/templates/lindale-diary.php
@@ -0,0 +1,21 @@
+<div class="row">
+    <div/class="col-md-12">
+
+        <h2 class="indented-h2">
+            Diary of events and meetings at the Lindale site of South Shore Methodist Community Church
+        </h2>
+
+        <div class = "table-responsive indented">
+            <table class="table table-bordered table-striped"
+                   summary="This table provides details of the events, services and meetings at the Lindale site of
+                        South Shore Methodist Community Church.">
+                <thead>
+
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
