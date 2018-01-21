15416b9f94cfbe5d6bfb93150eae890c3ce23f9f
Fiona Evans <f.e.evans@btinternet.com>
2018-01-21 11:34:38 +0000

    Amend font size and indentation on group pages

--- public/templates/lindale-groups.php
+++ public/templates/lindale-groups.php
@@ -1,14 +1,15 @@
 <div class="row">
-    <div/class="col-md-12">
+    <div class="col-md-12">
 
-    <h2 class="indented-h2">
-        Church groups at the Lindale site of South Shore Methodist Community Church
-    </h2>
+        <h2>
+            Church groups at the Lindale site of South Shore Methodist Community Church
+        </h2>
 
-    <div class="indented">
+        <div class="general-text">
 
-        <p>Details about each group that meets at Lindale or is associated with the church will be
-        presented here.</p>
+            <p>Details about each group that meets at Lindale or is associated with the church will be
+            presented here.</p>
+        </div>
+
     </div>
-</div>
 </div>
