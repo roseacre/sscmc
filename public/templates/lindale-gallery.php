ffe4524b34fc530f2bd87043ab8d262092ddcc16
Fiona Evans <f.e.evans@btinternet.com>
2018-01-21 11:38:55 +0000

    Amend font size and indentation on gallery pages

--- public/templates/lindale-gallery.php
+++ public/templates/lindale-gallery.php
@@ -1,13 +1,13 @@
 <div class="row">
-    <div/class="col-md-12">
+    <div class="col-md-12">
 
-    <h2 class="indented-h2">
-        Photo gallery for the Lindale site of South Shore Methodist Community Church
-    </h2>
+        <h2 >
+            Photo gallery for the Lindale site of South Shore Methodist Community Church
+        </h2>
 
-    <div class="indented">
+        <div class="general-text">
+            <p>Photos of Lindale Methodist Church or of people, groups or events at the site will be presented here</p>
+        </div>
 
-        <p>Photos of Lindale Methodist Church or of people, groups or events at the site will be presented here</p>
     </div>
-</div>
 </div>
