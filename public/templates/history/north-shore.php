47c4dba67d0afdbb1c61871264cf1cfed49d78b1
Fiona Evans <f.e.evans@btinternet.com>
2020-02-09 14:11:18 +0000

    Revisions for Blackpool Circuit following review - north shore history

---
+++ public/templates/history/north-shore.php
@@ -0,0 +1,49 @@
+<div class="row">
+	<div class="col-md-1"></div>
+    <div class="col-md-10">
+
+        <div>
+
+        	<img src="../images/history/north-shore-exterior.png" class="resized-image padded" alt="Photograph of North Shore Methodist Church.">
+        	<img src="../images/history/north-shore-interior.png" class="resized-image padded" alt="Photograph of interior of North Shore Methodist Church.">
+        	<img src="../images/history/north-shore-organ-pipes.jpg" class="resized-image padded" alt="Photograph of organ pipes.">
+        	<img src="../images/history/north-shore-window1.jpg" class="resized-image padded" alt="Photograph of stained glass window.">
+        	<img src="../images/history/north-shore-window2.jpg" class="resized-image padded" alt="Photograph of cross sihouetted in stained glass window.">
+
+			<p class="general-text space-above">1875 - a Trust was formed to purchase land bounded by Dickson Road and Cocker Street</p>
+			<p class="general-text">1888 – the Mount Pleasant Wesleyan Chapel opened on July 3rd 1888.
+
+			<p class="general-text">1899 - Major William Hinksman gave the Trustees a gift of the land adjoining the site.
+			A new Trust was formed on December 29th 1899 with the intention of building a new Church and school, utilising the whole
+			of the land.</p>
+
+			<p class="general-text">1906 - Foundation Stones were officially laid April 4th 1906 by
+			Alderman Joseph Heap J.P., Mrs. Farrar, Mr. R. Hughes, Mr. W. Robinson, Rev. H.R. Glanville, and Mr. Wm. Thompson J.P.
+			The Church was built by Messrs. S. Butterworth & Sons Ltd, the architect, a Mr. George Payton of Birmingham.</p>
+
+			<p class="general-text">1907 - Mrs. Wm. Thompson opened the Church on April 24th 1907, in the presence of
+			Rev. Albert Clayton, the president of the Wesleyan Methodist Church.</p>
+
+			<div class="panel-group space-above" id="accordion">
+
+				<div class="panel panel-default space-above">
+					<div class="panel-heading">
+						<h3 class="panel-title" id="panel-title-north-shore-gazette">
+							<a class="indented-subpage panel-title" data-toggle="collapse" data-parent="#accordion"
+							   href="#collapse-gazette">Blackpool Gazette: End of an era for North Shore Methodist Church  ▼</a>
+						</h3>
+					</div>
+					<div id="collapse-gazette" class="panel-collapse collapse">
+						<div class="panel-body">
+							<?php include '../templates/history/north-shore-closure-gazette.php'; ?>
+						</div>
+					</div>
+				</div>
+			</div>
+
+        </div>
+
+    </div>
+    <div class="col-md-1"></div>
+</div>
+
