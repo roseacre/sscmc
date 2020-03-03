0850bf69cf9962333a331323701fdd537c3d88c7
Fiona Evans <f.e.evans@btinternet.com>
2020-03-03 17:59:34 +0000

    amendments following 3rd review - add history

--- public/templates/history/raikes-parade.php
+++ public/templates/history/raikes-parade.php
@@ -30,3 +30,25 @@
     </div>
 </div>
 
+<div class="row">
+    <div class="col-md-6">
+    	<img src="../images/history/raikes-memorial1.jpg" class="resized-image padded"
+    		alt=" First World War bronze memorial plaque"
+    		title="First World memorial plaque, Raikes Parade Wesleyan Methodist Church">
+    </div>
+    <div class="col-md-6">
+    	<p class="general-text padded-top">First World War Memorial Plaque for Raikes Parade Wesleyan Methodist Church, with details on
+    	<a href="https://www.warmemorialsonline.org.uk/memorial/54907/">warmemorialsonline.org.uk</a></p>
+
+		<p class="general-text">It bears the inscription: ‘To the glory of God and sacred to the memory of the members of
+		this church and congregation who laid down their lives in the Great War 1914-1918’.</p>
+
+		<p class="general-text">The names of the fallen are listed as follows: George W Hargreaves, John Barrett, Alfred Coles,
+		Arthur M Cross, Duncan Clark, Joseph Dewhurst, Ben Howarth, Albert Lord, Frank Morris, Joseph Riding, Ben Riley,
+		Arthur Sykes, Harry Sykes.</p>
+
+		<p class="general-text">At the bottom of the plaque is the inscription ‘These all died in faith -desiring a better country.’</p>
+
+    </div>
+</div>
+
